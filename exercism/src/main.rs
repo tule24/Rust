6ea6cc3388e1617759b701ab5e4de7c752d56e0f
lecongtu <tulecong24@gmail.com>
2022-11-11 16:56:05 +0700

    update exercism

--- exercism/src/main.rs
+++ exercism/src/main.rs
@@ -1,3 +1,6 @@
+mod hello_world;
+mod lucian_luscious_lasagna;
+use crate::hello_world::hello_world::*;
+use crate::lucian_luscious_lasagna::lucian_luscious_lasagna::*;
 fn main() {
-    println!("Hello, world!");
 }
